1b6362f83ecb8d9ed0eff83b79fbbb196b87d23f
budoor <budooralsaidi1998@gmail.com>
2024-09-03 07:46:41 +0400

    first init commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorlddd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
